2824072c8f007422831edb8f824bb5a48b06a235
Perfight <m0nter1327@mail.ru>
2023-05-30 09:47:06 +0300

    fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,15 +8,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Pattern pattern = Pattern.compile("(((2[1-5]{2}|1?\\d{2}|\\d)\\.){3}((2[1-5]{2}|1?\\d{2}|\\d)))");
+        Pattern pattern = Pattern.compile("((((2[1-5]{2}|1?\\d{2}|\\d)\\.){3}((2[1-5]{2}|1?\\d{2}|\\d))))");
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("IP_Log.txt")))
         {
             System.out.println("Input the string");
             String str = in.nextLine();
             Matcher matcher = pattern.matcher(str);
-            if (matcher.find())
-                writer.write(matcher.group()  + " :  OK");
-            else {
+            String group;
+            if (matcher.find()) {
+                group = matcher.group();
+                if(str.equals(group)){
+                    writer.write(str + ": OK");
+                    return;
+                }
                 writer.write(str + " : Error");
             }
         } catch (IOException ex) {
