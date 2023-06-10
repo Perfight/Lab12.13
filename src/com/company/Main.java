8cc1ae774e61c4ef321fe45a842b10e1fedfadfa
Perfight <m0nter1327@mail.ru>
2023-06-10 10:02:06 +0300

    fix 3.0

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Pattern pattern = Pattern.compile("^((((2[1-5]{2}|1?\\d{2}|\\d)\\.){3}((2[1-5]{2}|1?\\d{2}|\\d))))$");
+        Pattern pattern = Pattern.compile("((\\d|1?\\d\\d|2([0-4]\\d|5[0-5]))\\.){3}(\\d|1?\\d\\d|2([0-4]\\d|5[0-5]))");
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("IP_Log.txt")))
         {
             System.out.println("Input the string");
